62564e175b134a153846aaadc3885aabaca8047b
nadesen <mononokehimehafukai@gmail.com>
2025-10-05 06:30:22 +0000

    10/5_Start

---
+++ app/models/book.rb
@@ -0,0 +1,21 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  validates :title,presence:true
+  validates :body,presence:true,length:{maximum:200}
+  has_many :book_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
+  def self.search_for(content, method)
+    if method == "perfect"
+      Book.where(title: content)
+    elsif method == "forward"
+      Book.where("title LIKE ?", content + "%")
+    elsif method == "backward"
+      Book.where("title LIKE ?", "%" + content)
+    else
+      Book.where("title LIKE ?", "%" + content + "%")
+    end
+  end
+end
