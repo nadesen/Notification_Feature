03bb8a8fe87e8e612e123f96ecc4ecdd9baa7831
nadesen <mononokehimehafukai@gmail.com>
2025-10-06 12:00:33 +0000

    10/6_Notification_Feature

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,14 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :notifications, as: :notifiable, dependent: :destroy
+
+  after_create do
+    user.followers.each do |follower|
+      Notification.create(user_id: follower.id, notifiable_type: “Book”, notifiable_id: id)
+    end
+  end  
+
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_09_13_055016) do
+ActiveRecord::Schema.define(version: 2025_10_06_073844) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -64,6 +64,17 @@ ActiveRecord::Schema.define(version: 2025_09_13_055016) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "notifiable_type", null: false
+    t.integer "notifiable_id", null: false
+    t.boolean "read"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["notifiable_type", "notifiable_id"], name: "index_notifications_on_notifiable"
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "relationships", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_id"
@@ -87,4 +98,5 @@ ActiveRecord::Schema.define(version: 2025_09_13_055016) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "notifications", "users"
 end
